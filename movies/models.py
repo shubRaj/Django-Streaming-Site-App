e3777268d270ceb230ace8db9a8bdc56689cfe0f
Shub Raj Lama <shuvraj1234@gmail.com>
2020-12-12 00:26:54 +0545

    image resize and remote image upload

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,12 @@ from django.utils.text import slugify
 from multiselectfield import MultiSelectField
 from django.core.validators import MaxValueValidator
 from django.contrib.auth.models import User
+from PIL import Image
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from django.conf import settings
+from pathlib import Path
+import os
 CATEGORY_CHOICES = (
     ("action","ACTION"),
     ("drama","DRAMA"),
@@ -31,9 +37,33 @@ STATUS_CHOICES = (
     ("featured","FEATURED"),
     ("mostwatched","MOST WATCHED"),
 )
+def resize_image(original_image,image_name,image_size):
+    image = Image.open(original_image)
+    if image.width>image_size[0] or image.height>image_size[1]:
+        image.thumbnail(image_size)
+        if "poster" in image_name:
+            image.save(Path(settings.MEDIA_ROOT)/f"poster/{image_name}")
+        else:
+            image.save(Path(settings.MEDIA_ROOT)/f"banner/{image_name}")
+        os.remove(original_image)
+def downloadImage(image_url,image_name,image_size):
+    r = requests.get(image_url)
+    size = round(int(r.headers.get("content-length"))/(1024*1024))
+    if size <=10:
+        imageBytes = r.content
+        original_image = r.headers.get("content-disposition").split('"')[-2]
+        image_extension = original_image.split(".")[-1]
+        image_name = f'{image_name}.{image_extension}'
+        original_image = Path(settings.MEDIA_ROOT)/original_image
+        with open(original_image,"wb") as f:
+            f.write(imageBytes)
+        resize_image(original_image,image_name,image_size)
+    return image_name
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     imdbID = models.IntegerField(null=True,blank=True,unique=True)
+    posterURL = models.URLField(max_length=2083,null=True,blank=True)
+    bannerURL = models.URLField(max_length=2083,null=True,blank=True)
     poster = models.ImageField(upload_to="poster",default="poster/poster.jpg")
     banner = models.ImageField(upload_to="banner",default="banner/banner.jpg")
     category = MultiSelectField(choices=CATEGORY_CHOICES,max_length=40,max_choices=4,null=True)
@@ -53,7 +83,14 @@ class Movie(models.Model):
         return f"{self.title}"
     def save(self,*args,**kwargs):
         if not self.slug:
-            self.slug = f"{slugify(self.title)}-full-movie-watch-online-hd-free-download"
+            self.slug = f"{slugify(self.title)}-{self.imdbID}"
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            if self.posterURL and self._meta.get_field("poster").default in self.poster.path:
+                posterThread = executor.submit(downloadImage,self.posterURL,f"{self.imdbID}-poster",(200,300))
+                self.poster = f"poster/{posterThread.result()}"
+            if self.bannerURL and self._meta.get_field("banner").default in self.banner.path:
+                bannerThread = executor.submit(downloadImage,self.bannerURL,f"{self.imdbID}-banner",(1280,720))
+                self.banner = f"banner/{bannerThread.result()}"
         super(Movie,self).save(*args,**kwargs)
 class Cast(models.Model):
     movie = models.ManyToManyField(Movie,related_name="movie_cast")
